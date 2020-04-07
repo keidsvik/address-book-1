c0945f751e1d27600f3a33bdbea20bb1f783a45a
Spencer Moody <spencer.moody@outlook.com>
2020-04-07 11:58:52 -0700

    Debugging everything
    co-authored-by: Kelly-Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -33,23 +33,29 @@ AddressBook.prototype.deleteContact = function(id) {
   };
   return false;
 }
-AddressBook.prototype.update = function(id){
- object.defineProperties(contacts)
-  
-}
 
 // Business logic for contacts -----
-function Contact(firstName, lastName, phoneNumber, email, address) {
+function Address(addressOne, addressTwo) {
+  this.addressOne = addressOne;
+  this.addressTwo= addressTwo;
+}
+
+function Contact(firstName, lastName, phoneNumber, email) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
   this.email = email;
-  this.address = address;
+  this.addresses = [];
+ 
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+Contact.prototype.addAddress = function(address) {
+  this.addresses.push(address);
+};
 
 // User Interface Logic ----------
 
@@ -58,11 +64,12 @@ var addressBook = new AddressBook();
 function displayContactDetails(addressBookToDisplay) {
   var contactsList = $("ul#contacts");
   var htmlForContactInfo = "";
+  console.log("CONTACTS: ", addressBookToDisplay.contacts);
   addressBookToDisplay.contacts.forEach(function(contact) {
     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName  + "</li>";
    });
    contactsList.html(htmlForContactInfo)
-   };
+};
 
 function showContact(contactId) {
   var contact = addressBook.findContact(contactId);
@@ -71,12 +78,21 @@ function showContact(contactId) {
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
   $(".email").html(contact.email);
-  $(".address").html(contact.address);
-  var buttons = $("#buttons");
-  buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+};
+
+function showAddress(addressId){
+  var address = addressBook.findAddress(addressId);
+  $("show-address").show();
+  $("#address-one").html(address.addressOne);
+  $("#address-two").html(address.addressTwo);
 }
-   
+
+
+ /* var buttons = $("#buttons"); {
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+} */
+
 function attachContactListeners() {
   $("ul#contacts").on("click", "li", function () {
      showContact(this.id);
@@ -97,16 +113,29 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedEmail = $("input#new-email").val();
-    var inputtedAddress = $("input#new-address").val();
+    var inputtedAddress1 = $("input#address-one").val();
+    var inputtedAddressType1 = $("input#address-type").val();
+    var inputtedAddress2 = $("input#address-two").val();
+    console.log("Input address: ", inputtedAddress2);
+    var inputtedAddressType2 = $("input#address-type2").val();
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email").val("");
-    $("input#new-address").val("");
+    $("input#address-one").val("");
+    $("input#address-type").val("");
+    $("input#address-two").val("");
+    $("input#address-type2").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail);
     addressBook.addContact(newContact);
+    var addressOne = new Address(inputtedAddress1, inputtedAddressType1);
+    var addressTwo = new Address(inputtedAddress2, inputtedAddressType2);
+    newContact.addAddress(addressOne);
+    newContact.addAddress(addressTwo);
+    console.log("Addresses after addContact: ", addressBook);
+    
     displayContactDetails(addressBook);
   })
 })
